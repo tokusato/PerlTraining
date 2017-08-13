ebe6e194a60c82b630c9238ed55773e591b12090
tokus <tokus@DESKTOP-J92QUTI>
2017-08-13 11:33:05 +0900

    added loop procedure

--- ZPerl/train/Directory.pl
+++ ZPerl/train/Directory.pl
@@ -3,3 +3,18 @@
 my $curdir = "getcwd";
 
 print("$curdir\n");
+
+#system関数を利用してOSコマンドを実行する。
+system("dir");
+
+#0埋めの5桁で数字を連続表示
+for (1..5){
+	my $strNum = sprintf("%05s",$_);
+	print ("$strNum\n");
+}
+
+
+
+
+
+
